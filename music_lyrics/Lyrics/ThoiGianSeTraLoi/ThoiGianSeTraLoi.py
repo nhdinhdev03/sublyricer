fa14cf228f70a526fab86abbab2f5e6cba33ec83
nhdinhdev03 <nhdinh.dev03@gmail.com>
2025-10-03 11:07:02 +0700

    thoi gian se tra loi

--- music_lyrics/Lyrics/ThoiGianSeTraLoi/ThoiGianSeTraLoi.py
+++ music_lyrics/Lyrics/ThoiGianSeTraLoi/ThoiGianSeTraLoi.py
@@ -23,20 +23,20 @@ def display(text, delay_after=0, char_speed=0.05, color="cyan"):
 
 
 lyrics = [
-    ("Nơi đâu cho em thật sự yên lành", 0.7, 0.05, "red"),
+    ("Nơi đâu cho em thật sự yên lành", 0.7, 0.05, "cyan"),
     ("Và ai đó mang em đi xa dần ", 0.6, 0.07, "red"),
     ("Lắng nghe tim anh đôi lần", 0.1, 0.06, "cyan"),
-    ("Xót xa khi em không cần mình gần bên nhau", 0.3, 0.06, "red"),
+    ("Xót xa khi em không cần mình gần bên nhau", 0.3, 0.07, "red"),
     ("Vì thời gian sẽ không trôi êm đềm ", 0.1, 0.07, "cyan"),
     ("Giống như khi đôi môi mềm", 0.2, 0.06, "red"),
     ("Còn ấm đam mê như ban đầu", 0.1, 0.06, "cyan"),
     ("Để miền yêu thương khép lại", 0.1, 0.06, "red"),
     ("Uhhh anh đã cố nói em khi", 0.4, 0.09, "cyan"),
-    ("Bên anh chỉ là mộng ước", 0.1, 0.05, "cyan"),
-    ("Nguội lạnh theo hơi ấm nhạt màu", 0.3, 0.07, "red"),
-    ("(Lạnh theo hơi ấm nhạt màu)", 0.2, 0.04, "cyan"),
-    ("Nguội lạnh theo hơi ấm nhạt màu", 0.3, 0.05, "red"),
-    ("(eh y-yeh y-yeh huh)", 0.3, 0.06, "cyan"),
+    ("Bên anh chỉ là mộng ước", 0.4, 0.08, "red"),
+    ("Nguội lạnh theo hơi ấm nhạt màu", 0.3, 0.07, "cyan"),
+    ("(Lạnh theo hơi ấm nhạt màu)", 0.2, 0.04, "red"),
+    ("Nguội lạnh theo hơi ấm nhạt màu", 0.3, 0.05, "cyan"),
+    ("(eh y-yeh y-yeh huh)", 0.3, 0.06, "red"),
 ]
 
 
